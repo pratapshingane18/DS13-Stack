a90e0a0f7338f7db9767624ff4d8bea088e17ad4
pratapshingane18 <sushrutrao465@gmail.com>
2023-01-14 15:25:49 +0530

    changes

---
+++ IntroStack.cpp
@@ -0,0 +1,32 @@
+/*
+        ***** STACK *****
+
+        LIFO- Last In First Out
+        The last element which was inserted is deleted/pop
+
+        Real Life:
+        1)
+         ___________________________
+        |car1 -> car2 ->car3 ...carn
+        |___________________________
+
+        Car n will get out first then car n-1 then ... car2 and lasy car1.
+
+        2) Given a Can and balls Indside it to get the ball
+         at the bottom we first have to take out all the balls above it.
+
+
+
+
+
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"Stack"<<endl;
+    return 0;
+}
