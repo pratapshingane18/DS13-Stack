4c4e8975ae1f1b7b3dec2ab9488e262e7b9c82e3
pratapshingane18 <sushrutrao465@gmail.com>
2023-01-17 22:43:08 +0530

    Stack with LL

--- StackUsingLL.cpp
+++ StackUsingLL.cpp
@@ -1,35 +1,105 @@
-/*
+// /*
 
+// */
 
+// #include<bits/stdc++.h>
+// using namespace std;
 
-*/
+// struct Node{
+//     int data;
+//     struct Node *next;
+// }top=NULL;
 
-#include<bits/stdc++.h>
-using namespace std;
+// void push(int x){
+//     struct Node *t;
+//     t = new Node;
 
+//     if(t == NULL){
+//         cout<<"Stack/Heap is FULL"<<endl;
+//     }
 
-struct Node{
+//     else{
+//         t->data =x;
+//         t->next = top;
+//         top =t;
+//     }
+// }
+
+// int pop(){
+//     if(top== NULL){
+//         cout<<"stack is empty"<<endl;
+//     }
+
+//     else{
+//         t = top;
+//         top = tp->next;
+//         x = t->data;
+
+//         free(t);
+//     }
+// }
+// int main(){
+
+//     return 0;
+// }
+
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
     int data;
     struct Node *next;
-}top=NULL;
-
-
-void push(int x){
+} *top = NULL;
+void push(int x)
+{
     struct Node *t;
-    t = new Node;
-    
-    if(t == NULL){
-        cout<<"Stack/Heap is FULL"<<endl;
-    }
+    t = (struct Node *)malloc(sizeof(struct Node));
 
-    else{
-        t->data =x;
+    if (t == NULL)
+        printf("stack is full\n");
+    else
+    {
+        t->data = x;
         t->next = top;
-        top =t;
+        top = t;
     }
 }
-int main(){
+int pop()
+{
+    struct Node *t;
+    int x = -1;
 
+    if (top == NULL)
+        printf("Stack is Empty\n");
+    else
+    {
+        t = top;
+        top = top->next;
+        x = t->data;
+        free(t);
+    }
+    return x;
+}
+void Display()
+{
+    struct Node *p;
+    p = top;
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+    printf("\n");
+}
+int main()
+{
+    push(10);
+    push(20);
+    push(30);
+
+    Display();
+
+    printf("%d ", pop());
 
     return 0;
 }
